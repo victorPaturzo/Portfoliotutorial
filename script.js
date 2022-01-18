454275eaabdb53da0b2c7a94a6f2afae44744d74
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2022-01-17 20:03:49 -0500

    Added teams section for future additions to resume.  Completed teams section. (VPP)

--- script.js
+++ script.js
@@ -11,5 +11,27 @@ $(document).ready(function(){
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i ').toggleClass("active");
-    })
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoPlayTimeOut: 2000,
+        autoplatHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false,
+            },
+            600:{
+                items: 2,
+                nav: false,
+            },
+            1000:{
+                items: 3,
+                nav: false,
+            }
+        }
+    });
 });
