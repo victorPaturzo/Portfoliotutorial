76a60ccb6d318c533a7346c74062ad0c73d8d965
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2022-01-17 22:16:13 -0500

    Finished portfolio frame.  Added stylization and js features to make page more exciting.  Moving forward with plugging in content. (VPP)

--- script.js
+++ script.js
@@ -5,12 +5,37 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
     });
 
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i ').toggleClass("active");
+    });
+
+    // typing animation script
+    let typed = new Typed(".typing", {
+        strings: ["MERN Developer", "Web Designer", "Coder"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    let typedTwo = new Typed(".typing-2", {
+        strings: ["MERN Developer", "Web Designer", "Coder"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     // owl carousel script
